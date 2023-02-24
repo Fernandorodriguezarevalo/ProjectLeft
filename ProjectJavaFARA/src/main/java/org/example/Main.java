e8dfb6d037d8eb46c5ce72e08da5992cc56f7a76
Fernandorodriguezarevalo <rodriguezarevalofernandoarturo@gmail.com>
2023-02-24 10:41:26 -0600

    Practica 1

--- ProjectJavaFARA/src/main/java/org/example/Main.java
+++ ProjectJavaFARA/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.w3c.dom.ls.LSOutput;
 
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class Main {
@@ -41,7 +42,7 @@ public class Main {
         // imc(índice de masa corporal) es peso/altura2 o peso/altura * altura y al final que muestre una ficha de todos esos datos
 
 
-        Scanner miescaner = new Scanner(System.in);
+        /*Scanner miescaner = new Scanner(System.in);
         String miTexto = "Nombre";
 
         String nombre;
@@ -61,7 +62,63 @@ public class Main {
         System.out.println("La edad del usuario es: " +edad);
         System.out.println("El peso del usuario es: " +peso);
         System.out.println("La altura del usuario es: " +altura);
-        System.out.println("El IMC del usuario es: " +IMC);
+        System.out.println("El IMC del usuario es: " +IMC);*/
+
+        //Desarrollar un programa que le pida al usuario su nombre, apellido, domicilio, cuanto cotizo en 2022, x
+        // calcular cuanto impuesto va a pagar.
+        //Declarar constante de ISR = 12%
+        //mostrar los datos al usuario
+
+        /*Scanner scanner = new Scanner(System.in);
+        String nom, apel, dom;
+        Double cotizo2022, impuestoApagar;
+        final double isr = 0.12;
+
+        System.out.println("Ingresa tu nombre: ");
+        nom = scanner.nextLine();
+        System.out.println("Ingresa tu apellido: ");
+        apel = scanner.nextLine();
+        System.out.println("Ingresa tu domicilio: ");
+        dom = scanner.nextLine();
+        System.out.println("Ingresa la cantidad que cotizaste en 2022: ");
+        cotizo2022 = scanner.nextDouble();
+
+        impuestoApagar = cotizo2022 * isr;
+
+        System.out.println("NOmbre: " + nom);
+        System.out.println("Apellido: " + apel);
+        System.out.println("Domicilio: " +dom);
+        System.out.println("Cotiazdo 2022: " + cotizo2022);
+        System.out.println("Impuesto a pagar (ISR): " + impuestoApagar);*/
+
+        //paso 1 pedir al usuario nombre, apellido, domicilio, precio de su carro
+        //paso 2 calcular el impuesto de tenencia vale 1.3% del costo del carro
+
+        Scanner scanner = new Scanner(System.in);
+        String nom, apel, dom;
+        double preciocarro;
+        final double impuestotenencia = 0.013;
+        double impuestoApagar;
+
+        System.out.println("Ingresa tu nombre: ");
+        nom = scanner.nextLine();
+        System.out.println("Ingresa tu apellido: ");
+        apel = scanner.nextLine();
+        System.out.println("Ingresa tu domicilio: ");
+        dom = scanner.nextLine();
+        System.out.println("Ingresa el precio de tu carro: ");
+        preciocarro =scanner.nextDouble();
+
+        impuestoApagar = preciocarro * impuestotenencia;
+
+        System.out.println("Nombre: " +nom);
+        System.out.println("Apellido: " +apel);
+        System.out.println("Domicilio: " +dom);
+        System.out.println("Preciocarro: " +preciocarro);
+        System.out.println("ImpuestoApagar:" +impuestoApagar);
+
+
+
 
 
 

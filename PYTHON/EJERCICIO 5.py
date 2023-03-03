80f69a3771f942da727eb88113ec44ee988ec85f
Fernandorodriguezarevalo <rodriguezarevalofernandoarturo@gmail.com>
2023-03-03 11:15:15 -0600

    Ejercicio 5

---
+++ PYTHON/EJERCICIO 5.py
@@ -0,0 +1,8 @@
+base = input ("Ingresa La base del triangulo: ")
+base = int (base)
+
+altura = input ("Ingresa La altura del triangulo: ")
+altura = int (altura)
+
+area = base * altura / 2 
+print ("El area es: ", area)
